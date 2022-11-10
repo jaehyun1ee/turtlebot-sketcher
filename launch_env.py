6f833f27b4a786f811c96299d6bd8e4a6a64c3df
dongjoo0-0 <kdj483647@gmail.com>
2022-11-11 01:36:10 +0900

    Fix it finally closes env

--- launch_env.py
+++ launch_env.py
@@ -4,31 +4,32 @@ import turtlebot3_env
 if __name__ == '__main__':
     # make environment
     env = gym.make('turtlebot3_env/Turtlebot3-v0')
+    try:
+        conti = True
+        while conti:
+            # reset environment
+            env.reset()
 
-    conti = True
-    while conti:
-        # reset environment
-        env.reset()
+            # a random policy
+            done = False
+            its = 0
+            while not done:
+                action = env.action_space.sample()
+                next_state, reward, done, _ = env.step(action)
+                print(f"it ({its});\nstate : {next_state}\naction : {action}\nreward : {reward}")
 
-        # a random policy
-        done = False
-        its = 0
-        while not done:
-            action = env.action_space.sample()
-            next_state, reward, done, _ = env.step(action)
-            print(f"it ({its});\nstate : {next_state}\naction : {action}\nreward : {reward}")
+                its += 1
+                if (its == 200):
+                    break
+            env.stop()
+            print(f"final : {env.state}")
 
-            its += 1
-            if (its == 200):
-                break
-        env.stop()
-        print(f"final : {env.state}")
+            # continue or not?
+            key = int(input("ENTER 1 TO CONTINUE : "))
+            if key != 1:
+                conti = False
 
-        # continue or not?
-        key = int(input("ENTER 1 TO CONTINUE : "))
-        if key != 1:
-            conti = False
-
-    # close environment
-    input("ENTER TO CLOSE")
-    env.close()
+        # close environment
+        input("ENTER TO CLOSE")
+    finally:
+        env.close()
